fc5df0a27b10264c07959d2bae318ea1960a4d79
Natan Dourado <nsdourado12@gmail.com>
2025-07-01 15:02:47 -0300

    add useSummary() in TransactionContext

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,32 +1,13 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { useContext } from "react";
-import { TransactionContext } from "../../contexts/TransactionsContexts";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 // Isto pode virar um map, quando eu estiver com os dados da API
 
 export function Summary() {
-  const { transactions } = useContext(TransactionContext);
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price
-
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
